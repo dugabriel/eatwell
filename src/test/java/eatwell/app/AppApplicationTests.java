b108f579c79698240f0ad28bc97f5f8e8dde17af
Eduardo Gabriel <eduardogabriel@Eduardos-MacBook-Air.local>
2024-07-11 20:13:56 -0300

    first commit

---
+++ src/test/java/eatwell/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package eatwell.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
